e0e7b64620f60befa4c2ad7258e157743b9de607
Jack <jbonomo38@gmail.com>
2024-09-18 23:42:44 -0400

    Initialize webpack

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "index.html",
+      inject: true
+    })
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"), // TODO: change to docs before pushing to github
+    publicPath: '/to-do/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
